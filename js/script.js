37d13b48b2d7595b49e9235e17cdab6213e1e989
unknown <juniorofdon@yahoo.fr>
2025-06-23 02:00:49 +0200

    Ajout du controle saisie - masquage de la fenetre de reponse au chargement - eviter les doublons de description

--- js/script.js
+++ js/script.js
@@ -1,60 +1,79 @@
+//Fonction récupérant les entrées en langage fr
+function entreeFr(entree) {
+    return entree.language.name == "fr";
+}
+
+//Garder que les valeurs unique d'un tableau
+function onlyUnique(value, index, array) {
+    return array.indexOf(value) === index;
+}
+
 $("#chercher").on("click", function () {
+    if ($("#numPokemon").val() <= 0 || $("#numPokemon").val() > 898 ) {
+        alert("Veuillez saisir un Id compris entre 1 et 898 inclus");
+    } else {
+        fetch("https://pokeapi.co/api/v2/pokemon-species/" + $("#numPokemon").val())
+            .then(response => response.json())
+            .then(response => {
+                $("#containerReponse").css("visibility", "visible");
+                
+                //Passer la couleur du cadre à la couleur du pokemon sélectionné
+                $("#cadreReponse").css("border", `3px solid ${response.color.name}`);
 
-    fetch("https://pokeapi.co/api/v2/pokemon-species/" + $("#numPokemon").val())
-        .then(response => response.json())
-        .then(response => {
-            // console.log(response);
-            console.log(response.flavor_text_entries);
+                //Récupération des différentes informations du pokemon sélectionné
+                $("#NumTitreH2").html($("#numPokemon").val());
+                $("#nomPokemon").html(response.names[4].name);
+                $("#txCapture").html(response.capture_rate);
+                $("#nomFamille").html(response.genera[3].genus);
 
-            const entrees = response.flavor_text_entries;
+                //Récupération des objets textes de description du pokemon selectionné
+                const entrees = response.flavor_text_entries;
+                //On filtre que les objets textes de description en français
+                const flavor_text_entries_fr = entrees.filter(entreeFr)
 
-            const flavor_text_entries_fr = entrees.filter(entreeFr)
-            function entreeFr(entree) {
-                return entree.language.name == "fr";
-            }
-            console.log(flavor_text_entries_fr);
+                //On crée un tableau regroupant que les textes de descriptions en fr
+                const flavor_text_fr = []
+                for (let i = 0; i < flavor_text_entries_fr.length; i++) {
+                    flavor_text_fr.push(flavor_text_entries_fr[i].flavor_text.replace("\n", " "));
+                }
 
-            const flavor_text_fr = []
-            for (let i = 0; i < flavor_text_entries_fr.length; i++) {
-                flavor_text_fr.push(flavor_text_entries_fr[i].flavor_text);
-            }
-            console.log(flavor_text_fr);
+                //On reformate chaque texte afin d'enlever les sauts de ligne /n
+                //Une même phrase sur 2 versions pouvant avoir des sauts de ligne différents (ex:Carapuce Id:7)
+                flavor_text_fr_reformat = []
+                for (let i = 0; i < flavor_text_fr.length; i++) {
+                    flavor_text_fr_reformat.push(flavor_text_fr[i].replace("\n", " "));
+                }
+                
+                //Une fois chaque phrase nettoyer de ces sauts de ligne, 
+                //on peut refaire un tableau avec des valeurs uniques
+                const unique_flavor_text_fr = flavor_text_fr_reformat.filter(onlyUnique);
+                
+                //On crée un paragraphe de description avec deux phrases différentes de 2 versions
+                //puis on l'affiche
+                descripParagraphe = unique_flavor_text_fr[0] + "<br/>" + unique_flavor_text_fr[1];
+                $("#description").html(descripParagraphe);
 
-            function onlyUnique(value, index, array) {
-                return array.indexOf(value) === index;
-            }
+                //Affichage de l'image du pokemon
+                $("#imgPokemon").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${$("#numPokemon").val()}.png`);
+            })
+            .catch(error => {
+                error = "Erreur dans votre requête AJAX !!!";
+                console.log("Erreur : " + error)
+            });
 
-            const unique_flavor_text_fr = flavor_text_fr.filter(onlyUnique);
-            console.log(unique_flavor_text_fr);
-            description1 = unique_flavor_text_fr[0].replace("\n", " ");
-            description2 = unique_flavor_text_fr[1].replace("\n", " ");
-            descripParagraphe = description1 + "<br/>" + description2;
-            $("#description").html(descripParagraphe);
+        // fetch("https://pokeapi.co/api/v2/pokemon/" + $("#numPokemon").val())
+        //     .then(resp => resp.json())
+        //     .then(resp => {
+        //         console.log(resp);
+        //         console.log(resp.sprites.other.official-artwork.front_default);
+        //     })
+        //     .catch(error => {
+        //         error = "Erreur dans votre requête AJAX !!!";
+        //         console.log("Erreur : " + error)
+        //     });
+    }
+        
 
-            $("#imgPokemon").attr("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${$("#numPokemon").val()}.png`);
-            
-            
-            
 
-            $("#cadreReponse").css("border", `3px solid ${response.color.name}`);
-            $("#NumTitreH2").html($("#numPokemon").val());
-            $("#nomPokemon").html(response.names[4].name);
-            $("#txCapture").html(response.capture_rate);
-            $("#nomFamille").html(response.genera[3].genus);
-        })
-        .catch(error => {
-            error = "Erreur dans votre requête AJAX !!!";
-            console.log("Erreur : " + error)
-        });
-
-    // fetch("https://pokeapi.co/api/v2/pokemon/" + $("#numPokemon").val())
-    //     .then(resp => resp.json())
-    //     .then(resp => {
-    //         console.log(resp);
-    //         console.log(resp.sprites.other.official-artwork.front_default);
-    //     })
-    //     .catch(error => {
-    //         error = "Erreur dans votre requête AJAX !!!";
-    //         console.log("Erreur : " + error)
-    //     });
+    
 });
